53bd07a212a3266ac71a34d90a086474dce04ead
krish <krishrajani680@gmail.com>
2026-03-23 22:02:26 -0600

    account model generation

---
+++ db/migrate/20260324035908_create_accounts.rb
@@ -0,0 +1,11 @@
+class CreateAccounts < ActiveRecord::Migration[8.1]
+  def change
+    create_table :accounts do |t|
+      t.string :name
+      t.string :currency
+      t.decimal :balance
+
+      t.timestamps
+    end
+  end
+end
